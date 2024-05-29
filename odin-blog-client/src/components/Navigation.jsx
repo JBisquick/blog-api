2bd713cc95987f2908bc27bdf78bdab355bb62a4
JBisquick <felipebirch@gmail.com>
2024-05-29 16:14:29 -0700

    Add logout function, validation token function, and changing navigation

--- odin-blog-client/src/components/Navigation.jsx
+++ odin-blog-client/src/components/Navigation.jsx
@@ -1,12 +1,37 @@
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import validateToken from '../hooks/validateToken';
 
 function Navigation() {
+  const [valid, setValid] = useState(false);
+
+  useEffect(() => {
+    setValid(validateToken());
+    console.log(valid);
+  },[])
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setValid(false);
+  };
+
+
   return (
-    <div>
-      <Link to='/'>Home </Link>
-      <Link to='/log-in'>Login </Link>
-      <Link to='/log-in'>Logout </Link>
-    </div>
+    <>
+    {valid === true ? (
+      <div>
+        <Link to='/'>Home </Link>
+        <button onClick={handleLogout}>Log Out</button>
+      </div>
+    ) : (
+      <div>
+        <Link to='/'>Home </Link>
+        <Link to='/log-in'>Login </Link>
+        <Link to='/sign-up'>Signup </Link>
+        <button onClick={handleLogout}>Log Out</button>
+      </div>
+    )}
+    </>
   );
 }
 
